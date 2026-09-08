db8b0ca158eba4c8060fb8087c12f492670ee5ec
ArunimaDas1 <kajaldas1462@gmail.com>
2026-09-08 23:43:20 +0530

    IdenticalTree: Same Tree

---
+++ IdenticalTree.java
@@ -0,0 +1,12 @@
+import javax.swing.tree.TreeNode;
+
+public class IdenticalTree(TreeNode p, TreeNode q){
+        if (p == null || q == null) {
+            return p == q;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }

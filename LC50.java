69540d441b41e400091251101c5ae17755028670
ArunimaDas1 <kajaldas1462@gmail.com>
2026-09-13 23:47:33 +0530

    LC50: Pow(x,n)

---
+++ LC50.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n; // Use long to prevent overflow when n = Integer.MIN_VALUE
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double ans = 1.0;
+        while (N > 0) {
+            if ((N & 1) == 1) { // If N is odd
+                ans *= x;
+            }
+            x *= x; // Square the base
+            N >>= 1; // Halve the exponent (N /= 2)
+        }
+
+        return ans;
+    }
+}

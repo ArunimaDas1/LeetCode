927822277b1242b0ddb1bd33ab3f8facde809134
ArunimaDas1 <kajaldas1462@gmail.com>
2026-09-14 23:54:59 +0530

    LC141: Linked List Cycle

---
+++ LC141.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;        // 1 step forward
+            fast = fast.next.next;   // 2 steps forward
+
+            if (slow == fast) {
+                return true; // Cycle detected
+            }
+        }
+
+        return false; // Fast reached end of list
+    }
+}
